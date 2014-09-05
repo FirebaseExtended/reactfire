69dcdc2666f11becbc3a6349c495bf703d440f37
TrySpace <info@cube.dj>
2014-09-05 04:54:29 +0200

    Added shorthand methods and 3rd Array args
    I just realize that it would have been better to have a different order
    in the array i.e. eventType first. However, I'd rather write [ true ]
    than [ 'value', true, false ].
    Some combinations won't be so common or allowed, so I think will do.
    To be able to have a .once() ability would be very nice for online games
    or large indexes of fast changing data, where you wouldn't wan't to
    update every ms, but rather every few seconds.

--- src/reactfire.js
+++ src/reactfire.js
@@ -22,20 +22,94 @@ var ReactFireMixin = {
   /*  BINDING  */
   /*************/
   /* Creates a binding between Firebase and the inputted bind variable as an array */
-  bindAsArray: function(firebaseRef, bindVar) {
-    this._bind(firebaseRef, bindVar, true);
+  bindAsArray: function(firebaseRef, bindVar, options) {
+    this._bind(firebaseRef, bindVar, true, options);
   },
 
   /* Creates a binding between Firebase and the inputted bind variable as an object */
-  bindAsObject: function(firebaseRef, bindVar) {
-    this._bind(firebaseRef, bindVar, false);
+  bindAsObject: function(firebaseRef, bindVar, options) {
+    this._bind(firebaseRef, bindVar, false, options);
   },
 
-  /* Creates a binding between Firebase and the inputted bind variable as either an array or object */
-  _bind: function(firebaseRef, bindVar, bindAsArray) {
-    this._validateBindVar(bindVar);
+  /* Creates a binding between Firebase and the inputted bind variable as an object or array */
+  /* On child_added  */
+  bindOnAdded: function(firebaseRef, bindVar, asArray) {
+  	options[0] = false
+  	options[1] = 'state'
+  	options[2] = 'child_added'
+  	asArray = (typeof asArray === 'boolean' ) ? asArray : false
+    this._bind(firebaseRef, bindVar, asArray, options);
+  },
 
-    var error;
+  /* Creates a binding between Firebase and the inputted bind variable as an object or array */
+  /* On child_removed  */
+  bindOnRemoved: function(firebaseRef, bindVar, asArray) {
+  	options[0] = false
+  	options[1] = 'state'
+  	options[2] = 'child_removed'
+  	asArray = (typeof asArray === 'boolean' ) ? asArray : false
+    this._bind(firebaseRef, bindVar, asArray, options);
+  },
+
+  /* Creates a binding between Firebase and the inputted bind variable as an object or array */
+  /* On child_changed  */
+  bindOnChanged: function(firebaseRef, bindVar, asArray) {
+  	options[0] = false
+  	options[1] = 'state'
+  	options[2] = 'child_changed'
+  	asArray = (typeof asArray === 'boolean' ) ? asArray : false
+    this._bind(firebaseRef, bindVar, asArray, options);
+  },
+
+  /* Creates a binding between Firebase and the inputted bind variable as an object or array */
+  /* On child_moved */
+  bindOnMoved: function(firebaseRef, bindVar, asArray) {
+  	options[0] = false
+  	options[1] = 'state'
+  	options[2] = 'child_moved'
+  	asArray = (typeof asArray === 'boolean' ) ? asArray : false
+    this._bind(firebaseRef, bindVar, asArray, options);
+  },
+
+  /* Creates a one-time exchange between Firebase and the inputted bind variable as an array */
+  onceAsArray: function(firebaseRef, bindVar, options) {
+  	options[1] = options[0]
+  	options[2] = options[1]
+  	options[0] = true
+    this._bind(firebaseRef, bindVar, true, options);
+  },
+
+  /* Creates a one-time exchange between Firebase and the inputted bind variable as an object */
+  onceAsObject: function(firebaseRef, bindVar, options) {
+  	options[1] = options[0]
+  	options[2] = options[1]
+  	options[0] = true
+    this._bind(firebaseRef, bindVar, false, options);
+  },
+
+  /* Creates a one-time props exchange between Firebase and the inputted bind variable as an array */
+  propAsArray: function(firebaseRef, bindVar, options) {
+  	options[2] = options[0]
+  	options[0] = true
+  	options[1] = 'props'
+    this._bind(firebaseRef, bindVar, true, options);
+  },
+
+  /* Creates a one-time props exchange between Firebase and the inputted bind variable as an object */
+  propAsObject: function(firebaseRef, bindVar, options) {
+  	options[2] = options[0]
+  	options[0] = true
+  	options[1] = 'props'
+    this._bind(firebaseRef, bindVar, false, options);
+  },
+
+
+  /* Creates a binding between Firebase and the inputted bind variable as either an array or object */
+  _bind: function(firebaseRef, bindVar, bindAsArray, options) {
+    this._validateBindVar(bindVar);
+    var error,
+        options = this._validateOptions(options)
+
     if (typeof firebaseRef.ref === "undefined" || firebaseRef.ref() instanceof Firebase === false) {
       error = "firebaseRef must be an instance of Firebase";
     }
@@ -48,16 +122,31 @@ var ReactFireMixin = {
     }
 
     this.firebaseRefs[bindVar] = firebaseRef.ref();
-    this.firebaseListeners[bindVar] = firebaseRef.on("value", function(dataSnapshot) {
-      var newState = {};
-      if (bindAsArray) {
-        newState[bindVar] = this._toArray(dataSnapshot.val());
-      }
-      else {
-        newState[bindVar] = dataSnapshot.val();
-      }
-      this.setState(newState);
-    }.bind(this));
+    if (!options.once) {
+
+	    this.firebaseListeners[bindVar] = firebaseRef.on(options.eventType, function(dataSnapshot) {
+			var newStore = {};
+			if (bindAsArray) {
+				newStore[bindVar] = this._toArray(dataSnapshot.val());
+			}
+			else {
+				newStore[bindVar] = dataSnapshot.val();
+			}
+			this.setState(newStore)
+		}.bind(this));
+	} else {
+
+	    this.firebaseListeners[bindVar] = firebaseRef.once(options.eventType, function(dataSnapshot) {
+	      	var newStore = {};
+			if (bindAsArray) {
+				newStore[bindVar] = this._toArray(dataSnapshot.val());
+			}
+			else {
+				newStore[bindVar] = dataSnapshot.val();
+			}
+	      	options.store ? this.setState(newStore) : this.setProps(newStore);
+	    }.bind(this));
+	}
   },
 
   /* Removes the binding between Firebase and the inputted bind variable */
@@ -123,5 +212,63 @@ var ReactFireMixin = {
       }
     }
     return out;
+  },
+
+  /* Validates options  */
+  _validateOptions: function(options){
+		var _defaultOptions = [false, 'state', 'value'];
+		options = (typeof options !== 'undefined' && this._isArray(options) ) ? options : _defaultOptions;
+		options[0] = this._validateOnce(options[0]) || _defaultOptions[0];
+		options[1] = this._validateType(options[1]) || this._validateType(_defaultOptions[1]); // returns true
+		options[2] = this._validateEventType(options[2]) || _defaultOptions[2];
+		options = this._validatePropState(options);
+		return {
+			once: options[0],
+			store: options[1],
+			eventType: options[2],
+		}
+  },
+
+  /* Validate if once is boolean */
+  _validateOnce: function(once){
+	if (typeof once !== "boolean") {
+      	throw new Error("ReactFire: 1st `once` option must be boolean; default: `false` " );
+    } else {
+    	return once;
+    }
+  },
+
+  /* Validate if type is string */
+  _validateType: function(type){
+  	if (typeof type !== "undefined" ) {
+  		return true
+  	}
+	else if (typeof type !== "string" || type.length === 0) {
+		// Forcing to write out 'state' or `props` out in code, for better readability
+      	throw new Error("ReactFire: 2nd `type` option must be either `state` or `props`; default: `state`");
+    } else {
+    	return (type === 'state');
+    }
+  },
+
+  /* Validate if type is string */
+  _validateEventType: function(eventType){
+  	if (typeof type !== "undefined" ) {
+  		return 'value'
+  	}
+	if (typeof eventType !== "string" || eventType.length === 0) {
+      	throw new Error('ReactFire: 3rd `eventType` option must be either "value", "child_added", "child_changed", "child_removed", or "child_moved".; default: `value`"');
+    } else {
+    	// Firebase should catch any unknown eventTypes
+    	return eventType;
+    }
+  },
+  /* Validate if no on listener is attempted on props  */
+  _validatePropState: function(options){
+  	var error;
+  	if (options[0] === false && options[1] === false) {
+  		 throw new Error("ReactFire: binding to props is considered bad practice in React, any frequent changing to props should probably be state!");
+  	}
+  	return options;
   }
 };
