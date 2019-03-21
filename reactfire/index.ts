b589e1bc5c9219a4e907abd28aae7ef7c0f77268
jhuleatt <jhuleatt@gmail.com>
2019-03-20 18:15:52 -0700

    hooks with suspense seem to be working??!!??

--- reactfire/index.ts
+++ reactfire/index.ts
@@ -1,30 +1,63 @@
 import * as React from 'react';
 import { user } from 'rxfire/auth';
-import { auth, User } from 'firebase/app';
-import { collection } from 'rxfire/firestore';
-import { first, map } from 'rxjs/operators';
+import { auth, User, firestore } from 'firebase/app';
+import { collection, doc } from 'rxfire/firestore';
+import { first, startWith } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
-export function useUser(auth: auth.Auth): User {
-  return useObservable(user(auth));
+const ongoingPromises = new Map();
+
+function isPromise(obj: any) {
+  return obj && obj.then;
 }
 
-export function useObservable(observable$: Observable<any>) {
-  const [latestValue, setValue] = React.useState(null);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const activePromise = React.useRef();
+export function useUser(auth: auth.Auth): User {
+  return useObservable(user(auth), 'user');
+}
 
-  if (isLoading) {
-    throw activePromise.current;
+export function suspendUntilFirst(observable$, observableId) {
+  let request = ongoingPromises.get(observableId);
+
+  if (!request) {
+    request = {
+      promise: observable$.pipe(first()).toPromise(),
+      isComplete: false
+    };
+    ongoingPromises.set(observableId, request);
   }
 
+  if (request.isComplete === false) {
+    throw request.promise.then(dataSnapShot => {
+      request.isComplete = true;
+      request.value = dataSnapShot;
+    });
+  }
+
+  return request.value;
+}
+
+export function useFirestoreDoc(ref: firestore.DocumentReference) {
+  return useObservable(doc(ref), ref.path);
+}
+
+export function useObservable(
+  observable$: Observable<any>,
+  observableId: string
+) {
+  const initialValue = suspendUntilFirst(observable$, observableId);
+
+  const [latestValue, setValue] = React.useState(initialValue);
+
   React.useEffect(() => {
-    activePromise.current = observable$.pipe(first()).toPromise();
-    setIsLoading(true);
-    const subscription = observable$.subscribe(
-      u => {
-        setIsLoading(false);
-        setValue(u);
+    const subscription = observable$.pipe(startWith(initialValue)).subscribe(
+      newVal => {
+        if (newVal === null) {
+          console.log('NULL VALUE RECEIVED');
+        }
+        console.log('NEW VALUE', newVal);
+        console.log('NEW VALUE', newVal.data);
+
+        setValue(newVal);
       },
       error => {
         console.warn('There was an error', error);
@@ -33,7 +66,7 @@ export function useObservable(observable$: Observable<any>) {
     );
 
     return subscription.unsubscribe;
-  }, []);
+  }, [observableId]);
 
   return latestValue;
 }
--- sample-simple/src/FirestoreCounter.js
+++ sample-simple/src/FirestoreCounter.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useFirestoreDoc, suspendOnDocumentReference } from 'reactfire';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const Counter = props => {
+  const ref = firebase.firestore().doc('count/counter');
+
+  const snapshot = useFirestoreDoc(ref);
+  console.log(snapshot);
+  const counterValue = snapshot.data().value;
+
   return (
     <>
       <button onClick={() => console.log('click')}>-</button>
-      <span> 0 </span>
+      <span> {counterValue} </span>
       <button onClick={() => console.log('click')}>+</button>
     </>
   );
 };
 
-export default Counter;
+const SuspenseWrapper = props => {
+  return (
+    <Suspense fallback="loading...">
+      <Counter />
+    </Suspense>
+  );
+};
+
+export default SuspenseWrapper;
