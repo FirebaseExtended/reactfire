30220ccb93b7262d478c70ad6810782c89ad71b0
jhuleatt <jhuleatt@gmail.com>
2019-03-21 15:51:50 -0700

    make sure user is signed in before Firestore read

--- sample-simple/src/FirestoreCounter.js
+++ sample-simple/src/FirestoreCounter.js
@@ -1,10 +1,17 @@
 import React, { Suspense } from 'react';
-import { useFirestoreDoc, suspendOnDocumentReference } from 'reactfire';
+import { useFirestoreDoc, useUser } from 'reactfire';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const Counter = props => {
   const ref = firebase.firestore().doc('count/counter');
+
+  const increment = amountToIncrement => {
+    ref.update({
+      value: firebase.firestore.FieldValue.increment(amountToIncrement)
+    });
+  };
 
   const snapshot = useFirestoreDoc(ref);
   console.log(snapshot);
@@ -12,17 +19,28 @@ const Counter = props => {
 
   return (
     <>
-      <button onClick={() => console.log('click')}>-</button>
+      <button onClick={() => increment(-1)}>-</button>
       <span> {counterValue} </span>
-      <button onClick={() => console.log('click')}>+</button>
+      <button onClick={() => increment(1)}>+</button>
     </>
   );
+};
+
+const AuthCheck = props => {
+  const user = useUser(firebase.auth());
+  if (!user) {
+    return props.fallback;
+  } else {
+    return props.children;
+  }
 };
 
 const SuspenseWrapper = props => {
   return (
     <Suspense fallback="loading...">
-      <Counter />
+      <AuthCheck fallback="sign in to use Firestore">
+        <Counter />
+      </AuthCheck>
     </Suspense>
   );
 };
