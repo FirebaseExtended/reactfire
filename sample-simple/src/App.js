29a1b4d12f0bc910e78782cb35c5881eddd4d17c
jhuleatt <jhuleatt@gmail.com>
2019-04-04 14:21:44 -0700

    turn samples into "cards"

--- sample-simple/src/App.js
+++ sample-simple/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AuthButton from './Auth';
 import FirestoreCounter from './Firestore';
+import Storage from './Storage';
 
 const Fire = () => (
   <span role="img" aria-label="Fire">
@@ -8,20 +9,36 @@ const Fire = () => (
   </span>
 );
 
+const Card = ({ title, children }) => {
+  return (
+    <div className="card">
+      <h1>
+        {title} <Fire />
+      </h1>
+      {children}
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <>
       <h1>
         <Fire /> ReactFire Demo <Fire />
       </h1>
-      <h2>
-        <Fire /> Authentication
-      </h2>
-      <AuthButton />
-      <h2>
-        <Fire /> Firestore
-      </h2>
-      <FirestoreCounter />
+      <div className="container">
+        <Card title="Authentication">
+          <AuthButton />
+        </Card>
+
+        <Card title="Firestore">
+          <FirestoreCounter />
+        </Card>
+
+        <Card title="Storage">
+          <Storage />
+        </Card>
+      </div>
     </>
   );
 };
