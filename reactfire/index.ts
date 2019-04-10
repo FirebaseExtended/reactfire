3cef83adc769710306b428b029fcc32c2653aa94
jhuleatt <jhuleatt@gmail.com>
2019-04-09 17:32:49 -0700

    add ability to pass initial value to all hooks

--- reactfire/index.ts
+++ reactfire/index.ts
@@ -6,30 +6,56 @@ import { useObservable } from './util/use-observable';
 import { getDownloadURL } from 'rxfire/storage';
 import { Observable, from } from 'rxjs';
 
+export interface ReactFireOptions {
+  startWithValue: any;
+}
+
 export function useUser(auth: auth.Auth): User {
   return useObservable(user(auth), 'user');
 }
 
 export function useFirestoreDoc(
-  ref: firestore.DocumentReference
+  ref: firestore.DocumentReference,
+  options?: ReactFireOptions
 ): firestore.DocumentSnapshot {
-  return useObservable(doc(ref), ref.path);
+  return useObservable(
+    doc(ref),
+    ref.path,
+    options ? options.startWithValue : undefined
+  );
 }
 
 export function useFirestoreCollection(
-  ref: firestore.CollectionReference
+  ref: firestore.CollectionReference,
+  options?: ReactFireOptions
 ): firestore.QuerySnapshot {
-  return useObservable(fromCollectionRef(ref), ref.path);
+  return useObservable(
+    fromCollectionRef(ref),
+    ref.path,
+    options ? options.startWithValue : undefined
+  );
 }
 
-export function useDatabaseObject(ref: database.Reference): QueryChange {
-  return useObservable(object(ref), ref.toString());
+export function useDatabaseObject(
+  ref: database.Reference,
+  options?: ReactFireOptions
+): QueryChange {
+  return useObservable(
+    object(ref),
+    ref.toString(),
+    options ? options.startWithValue : undefined
+  );
 }
 
 export function useDatabaseList(
-  ref: database.Reference | database.Query
+  ref: database.Reference | database.Query,
+  options?: ReactFireOptions
 ): QueryChange[] {
-  return useObservable(list(ref), ref.toString());
+  return useObservable(
+    list(ref),
+    ref.toString(),
+    options ? options.startWithValue : undefined
+  );
 }
 
 export function _fromTask(task: storage.UploadTask) {
@@ -50,11 +76,23 @@ export function _fromTask(task: storage.UploadTask) {
 
 export function useStorageTask(
   task: storage.UploadTask,
-  ref: storage.Reference
+  ref: storage.Reference,
+  options?: ReactFireOptions
 ): storage.UploadTaskSnapshot {
-  return useObservable(_fromTask(task), 'upload' + ref.toString());
+  return useObservable(
+    _fromTask(task),
+    'upload' + ref.toString(),
+    options ? options.startWithValue : undefined
+  );
 }
 
-export function useStorageDownloadURL(ref: storage.Reference): string {
-  return useObservable(getDownloadURL(ref), 'download' + ref.toString());
+export function useStorageDownloadURL(
+  ref: storage.Reference,
+  options?: ReactFireOptions
+): string {
+  return useObservable(
+    getDownloadURL(ref),
+    'download' + ref.toString(),
+    options ? options.startWithValue : undefined
+  );
 }
