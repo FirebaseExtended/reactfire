6256f8650a65978518b069621bf691d8646904b9
jhuleatt <jhuleatt@gmail.com>
2019-04-19 14:34:14 -0700

    add a test to make sure hook fires new values over time

--- reactfire/util/use-observable.test.tsx
+++ reactfire/util/use-observable.test.tsx
@@ -1,6 +1,6 @@
 import { useObservable } from './use-observable';
 import { renderHook, cleanup, act } from 'react-hooks-testing-library';
-import { of, Subject, observable } from 'rxjs';
+import { of, Subject, Observable, observable } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import {
   render,
@@ -15,7 +15,7 @@ import 'jest-dom/extend-expect';
 afterEach(cleanup);
 
 it('throws a promise if the observable has no initial value', () => {
-  const observable$ = new Subject();
+  const observable$: Subject<any> = new Subject();
 
   try {
     useObservable(observable$, 'test');
@@ -27,7 +27,7 @@ it('throws a promise if the observable has no initial value', () => {
 it('can return a startval and then the observable once it is ready', () => {
   const startVal = 'howdy';
   const observableVal = "y'all";
-  const observable$ = new Subject();
+  const observable$: Subject<any> = new Subject();
 
   const { result, waitForNextUpdate } = renderHook(() =>
     useObservable(observable$, 'test', startVal)
@@ -35,7 +35,7 @@ it('can return a startval and then the observable once it is ready', () => {
 
   expect(result.current).toEqual(startVal);
 
-  // just to prove that it actually does emit the value from the observable too
+  // prove that it actually does emit the value from the observable too
   act(() => observable$.next(observableVal));
   expect(result.current).toEqual(observableVal);
 });
@@ -61,6 +61,7 @@ it('works with Suspense', async () => {
   const FallbackComponent = () => (
     <h1 data-testid={fallbackComponentId}>Fallback</h1>
   );
+
   const Component = () => {
     const val = useObservable(observable$, 'test-suspense');
     return <h1 data-testid={actualComponentId}>{val}}</h1>;
@@ -72,14 +73,53 @@ it('works with Suspense', async () => {
     </React.Suspense>
   );
 
+  // make sure Suspense renders the fallback component if the observable has not emitted a value
   expect(getByTestId(fallbackComponentId)).toBeInTheDocument();
   expect(queryByTestId(actualComponentId)).toBeNull();
 
-  observable$.next(observableFinalVal);
-
+  act(() => observable$.next(observableFinalVal));
   await waitForElement(() => getByTestId(actualComponentId));
 
+  // make sure Suspense correctly renders its child after the observable emits a value
   expect(getByTestId(actualComponentId)).toBeInTheDocument();
   expect(getByTestId(actualComponentId)).toHaveTextContent(observableFinalVal);
   expect(queryByTestId(fallbackComponentId)).toBeNull();
+});
+
+it('updates the component as the observable emits new values', async () => {
+  const observableFirstValue = 'a';
+  const observableSecondValue = 'b';
+  const observable$ = new Subject();
+  const actualComponentId = 'actual-component';
+  const fallbackComponentId = 'fallback-component';
+
+  const FallbackComponent = () => (
+    <h1 data-testid={fallbackComponentId}>Fallback</h1>
+  );
+
+  const Component = () => {
+    const val = useObservable(observable$, 'test-suspense');
+    return <h1 data-testid={actualComponentId}>{val}}</h1>;
+  };
+
+  const { queryByTestId, getByTestId } = render(
+    <React.Suspense fallback={<FallbackComponent />}>
+      <Component />
+    </React.Suspense>
+  );
+
+  act(() => observable$.next(observableFirstValue));
+  await waitForElement(() => getByTestId(actualComponentId));
+
+  // check that the value matches
+  expect(getByTestId(actualComponentId)).toHaveTextContent(
+    observableFirstValue
+  );
+
+  act(() => observable$.next(observableSecondValue));
+
+  // check that the component was updated with the new value
+  expect(getByTestId(actualComponentId)).toHaveTextContent(
+    observableSecondValue
+  );
 });
