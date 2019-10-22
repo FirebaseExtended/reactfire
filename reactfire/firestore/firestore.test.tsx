72182e4b573597dec0d67c8ac1f93c7fbc5cbd28
jhuleatt <jhuleatt@gmail.com>
2019-10-21 17:16:45 -0700

    better observable ids

--- reactfire/firestore/firestore.test.tsx
+++ reactfire/firestore/firestore.test.tsx
@@ -1,5 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
-import { render, waitForElement, cleanup } from '@testing-library/react';
+import { render, waitForElement, cleanup, act } from '@testing-library/react';
 
 import * as React from 'react';
 import '@testing-library/jest-dom/extend-expect';
@@ -105,9 +104,6 @@ describe('Firestore', () => {
     });
   });
 
-  // THIS TEST CAUSES A REACT `act` WARNING
-  // IT WILL BE FIXED IN REACT 16.9
-  // More info here: https://github.com/testing-library/react-testing-library/issues/281
   describe('useFirestoreCollection', () => {
     it('can get a Firestore collection [TEST REQUIRES EMULATOR]', async () => {
       const mockData1 = { a: 'hello' };
@@ -115,8 +111,8 @@ describe('Firestore', () => {
 
       const ref = app.firestore().collection('testCollection');
 
-      await ref.add(mockData1);
-      await ref.add(mockData2);
+      await act(() => ref.add(mockData1));
+      await act(() => ref.add(mockData2));
 
       const ReadFirestoreCollection = () => {
         const collection = useFirestoreCollection(ref);
@@ -143,11 +139,45 @@ describe('Firestore', () => {
 
       expect(getAllByTestId('listItem').length).toEqual(2);
     });
+
+    it('Returns different data for different queries on the same path [TEST REQUIRES EMULATOR]', async () => {
+      const mockData1 = { a: 'hello' };
+      const mockData2 = { a: 'goodbye' };
+
+      const ref = app.firestore().collection('testCollection');
+      const filteredRef = ref.where('a', '==', 'hello');
+
+      await act(() => ref.add(mockData1));
+      await act(() => ref.add(mockData2));
+
+      const ReadFirestoreCollection = () => {
+        const list = (useFirestoreCollection(ref) as firestore.QuerySnapshot)
+          .docs;
+        const filteredList = (useFirestoreCollection(
+          filteredRef
+        ) as firestore.QuerySnapshot).docs;
+
+        // filteredList's length should be 1 since we only added one value that matches its query
+        expect(filteredList.length).toEqual(1);
+
+        // the full list should be bigger than the filtered list
+        expect(list.length).toBeGreaterThan(filteredList.length);
+
+        return <h1 data-testid="rendered">Hello</h1>;
+      };
+
+      const { getByTestId } = render(
+        <FirebaseAppProvider firebase={app}>
+          <React.Suspense fallback={<h1 data-testid="fallback">Fallback</h1>}>
+            <ReadFirestoreCollection />
+          </React.Suspense>
+        </FirebaseAppProvider>
+      );
+
+      await waitForElement(() => getByTestId('rendered'));
+    });
   });
 
-  // THIS TEST CAUSES A REACT `act` WARNING
-  // IT WILL BE FIXED IN REACT 16.9
-  // More info here: https://github.com/testing-library/react-testing-library/issues/281
   describe('useFirestoreCollectionData', () => {
     it('can get a Firestore collection [TEST REQUIRES EMULATOR]', async () => {
       const mockData1 = { a: 'hello' };
@@ -155,8 +185,8 @@ describe('Firestore', () => {
 
       const ref = app.firestore().collection('testCollection');
 
-      await ref.add(mockData1);
-      await ref.add(mockData2);
+      await act(() => ref.add(mockData1));
+      await act(() => ref.add(mockData2));
 
       const ReadFirestoreCollection = () => {
         const list = useFirestoreCollectionData<any>(ref, { idField: 'id' });
@@ -182,6 +212,42 @@ describe('Firestore', () => {
       await waitForElement(() => getAllByTestId('listItem'));
 
       expect(getAllByTestId('listItem').length).toEqual(2);
+    });
+
+    it('Returns different data for different queries on the same path [TEST REQUIRES EMULATOR]', async () => {
+      const mockData1 = { a: 'hello' };
+      const mockData2 = { a: 'goodbye' };
+
+      const ref = app.firestore().collection('testCollection');
+      const filteredRef = ref.where('a', '==', 'hello');
+
+      await act(() => ref.add(mockData1));
+      await act(() => ref.add(mockData2));
+
+      const ReadFirestoreCollection = () => {
+        const list = useFirestoreCollectionData<any>(ref, { idField: 'id' });
+        const filteredList = useFirestoreCollectionData<any>(filteredRef, {
+          idField: 'id'
+        });
+
+        // filteredList's length should be 1 since we only added one value that matches its query
+        expect(filteredList.length).toEqual(1);
+
+        // the full list should be bigger than the filtered list
+        expect(list.length).toBeGreaterThan(filteredList.length);
+
+        return <h1 data-testid="rendered">Hello</h1>;
+      };
+
+      const { getByTestId } = render(
+        <FirebaseAppProvider firebase={app}>
+          <React.Suspense fallback={<h1 data-testid="fallback">Fallback</h1>}>
+            <ReadFirestoreCollection />
+          </React.Suspense>
+        </FirebaseAppProvider>
+      );
+
+      await waitForElement(() => getByTestId('rendered'));
     });
   });
 });
