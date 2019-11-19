854b2d2ee2a3168d063fea9b1451422c636359ad
James Daniels <jamesdaniels@google.com>
2019-11-18 16:46:01 -0800

    Publish release candidates under the NPM latest tag

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 cd reactfire/pub/reactfire
 
+# TODO once we go 2.0.0 stable, uncomment
+# LATEST_TEST="^[^-]*$"
+
+# Publish -rc tags on @latest for now
+LATEST_TEST="^[^-]*-rc"
+
 if test $NPM_TOKEN; then
 
     echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
@@ -10,7 +16,14 @@ if test $NPM_TOKEN; then
 
     if test $TAG_NAME; then
         npm version $(echo $TAG_NAME | sed 's/^v\(.*\)$/\1/')
-        npm publish . --tag next
+
+        if [[ $TAG_NAME =~ $LATEST_TEST]]; then
+            NPM_TAG=latest
+        else
+            NPM_TAG=next
+        fi
+
+        npm publish . --tag $NPM_TAG
         ret=$?
     else
         npm version $(npm version | sed -n "s/^  reactfire: '\(.*\)',/\1/p")-canary.$SHORT_SHA
