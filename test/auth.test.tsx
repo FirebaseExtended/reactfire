2b584148eb8e0f797e5213cbcd358affc2914d33
jhuleatt <jhuleatt@gmail.com>
2020-11-20 14:18:34 -0800

    use the auth emulator

--- test/auth.test.tsx
+++ test/auth.test.tsx
@@ -1,146 +1,151 @@
 import { cleanup, render, waitFor } from '@testing-library/react';
-import firebase from 'firebase/app';
+import firebase from 'firebase';
 import '@testing-library/jest-dom/extend-expect';
 import * as React from 'react';
 import { FirebaseAppProvider, AuthCheck, useUser } from '..';
-import { Observer } from 'rxjs';
 import { act } from 'react-dom/test-utils';
+import { baseConfig } from './appConfig';
 
-class MockAuth {
-  user: Object | null;
-  subscriber: Observer<any> | null;
-  constructor() {
-    this.user = null;
-    this.subscriber = null;
-  }
+describe('Authentication', () => {
+  let app: firebase.app.App;
+  let signIn: () => Promise<firebase.auth.UserCredential>;
 
-  app = {
-    name: '[DEFAULT]'
-  };
+  const Provider = ({ children }: { children: React.ReactNode }) => (
+    <FirebaseAppProvider firebaseApp={(app as any) as firebase.app.App}>{children}</FirebaseAppProvider>
+  );
 
-  notifySubscriber() {
-    if (this.subscriber) {
-      this.subscriber.next(this.user);
-    }
-  }
+  const Component = (props?: { children?: any }) => (
+    <Provider>
+      <React.Suspense fallback={'loading'}>
+        <AuthCheck fallback={<h1 data-testid="signed-out">not signed in</h1>}>{props?.children || <h1 data-testid="signed-in">signed in</h1>}</AuthCheck>
+      </React.Suspense>
+    </Provider>
+  );
 
-  onIdTokenChanged(s: Observer<any>) {
-    this.subscriber = s;
-    this.notifySubscriber();
-  }
+  beforeAll(() => {
+    //@ts-ignore
+    window.fetch = nodeFetch;
 
-  updateUser(u: Object | null) {
-    this.user = u;
-    this.notifySubscriber();
-  }
-}
+    app = firebase.initializeApp(baseConfig);
 
-const mockAuth = new MockAuth();
+    // useEmulator emits a warning, which adds noise to test output. So, we get rid of console.warn for a moment
+    const realWarn = console.warn;
+    console.warn = jest.fn();
+    app.auth().useEmulator('http://localhost:9099/');
+    console.warn = realWarn;
 
-const mockFirebase = {
-  auth: () => mockAuth
-};
-
-const Provider = ({ children }: { children: React.ReactNode }) => (
-  <FirebaseAppProvider firebaseApp={(mockFirebase as any) as firebase.app.App}>{children}</FirebaseAppProvider>
-);
-
-const Component = (props?: { children?: any }) => (
-  <Provider>
-    <React.Suspense fallback={'loading'}>
-      <AuthCheck fallback={<h1 data-testid="signed-out">not signed in</h1>}>{props?.children || <h1 data-testid="signed-in">signed in</h1>}</AuthCheck>
-    </React.Suspense>
-  </Provider>
-);
-
-describe('AuthCheck', () => {
-  beforeEach(() => {
-    // clear the signed in user
-    act(() => mockFirebase.auth().updateUser(null));
-  });
-
-  afterEach(() => {
-    act(() => {
-      cleanup();
-      jest.clearAllMocks();
-    });
-  });
-
-  it('can find firebase Auth from Context', () => {
-    expect(() => render(<Component />)).not.toThrow();
-  });
-
-  it('can use firebase Auth from props', () => {
-    expect(() =>
-      render(
-        <React.Suspense fallback={'loading'}>
-          <AuthCheck fallback={<h1>not signed in</h1>} auth={(mockFirebase.auth() as unknown) as firebase.auth.Auth}>
-            {'signed in'}
-          </AuthCheck>
-        </React.Suspense>
-      )
-    ).not.toThrow();
-  });
-
-  it('renders the fallback if a user is not signed in', async () => {
-    const { getByTestId } = render(<Component />);
-
-    await waitFor(() => expect(getByTestId('signed-out')).toBeInTheDocument());
-
-    act(() => mockFirebase.auth().updateUser({ uid: 'testuser' }));
-
-    await waitFor(() => expect(getByTestId('signed-in')).toBeInTheDocument());
-  });
-
-  it('renders children if a user is logged in', async () => {
-    act(() => mockFirebase.auth().updateUser({ uid: 'testuser' }));
-    const { getByTestId } = render(<Component />);
-
-    await waitFor(() => expect(getByTestId('signed-in')).toBeInTheDocument());
-  });
-
-  it('can switch between logged in and logged out', async () => {
-    const { getByTestId } = render(<Component />);
-
-    await waitFor(() => expect(getByTestId('signed-out')).toBeInTheDocument());
-
-    act(() => mockFirebase.auth().updateUser({ uid: 'testuser' }));
-
-    await waitFor(() => expect(getByTestId('signed-in')).toBeInTheDocument());
-
-    act(() => mockFirebase.auth().updateUser(null));
-
-    await waitFor(() => expect(getByTestId('signed-out')).toBeInTheDocument());
-  });
-
-  test.todo('checks requiredClaims');
-});
-
-describe('useUser', () => {
-  it('always returns a user if inside an <AuthCheck> component', () => {
-    const UserDetails = () => {
-      const user = useUser();
-
-      expect(user).not.toBeNull();
-      expect(user).toBeDefined();
-
-      return <h1>Hello</h1>;
+    signIn = async () => {
+      return app
+        .auth()
+        .signInWithCredential(firebase.auth.GoogleAuthProvider.credential('{"sub": "abc123", "email": "foo@example.com", "email_verified": true}'));
     };
-
-    render(
-      <>
-        <Component>
-          <UserDetails />
-        </Component>
-      </>
-    );
-
-    act(() => mockFirebase.auth().updateUser({ uid: 'testuser' }));
   });
 
-  test.todo('can find firebase.auth() from Context');
+  test('sanity check - emulator is running', async () => {
+    // IF THIS TEST FAILS, MAKE SURE YOU'RE RUNNING THESE TESTS BY DOING:
+    // yarn test
 
-  test.todo('throws an error if firebase.auth() is not available');
+    const user = await signIn();
 
-  test.todo('returns the same value as firebase.auth().currentUser()');
+    expect(user).toBeDefined();
+  });
+
+  describe('AuthCheck', () => {
+    beforeEach(async () => {
+      // clear the signed in user
+      await act(async () => {
+        await app.auth().signOut();
+      });
+    });
+
+    afterEach(() => {
+      act(() => {
+        cleanup();
+        jest.clearAllMocks();
+      });
+    });
+
+    it('can find firebase Auth from Context', () => {
+      expect(() => render(<Component />)).not.toThrow();
+    });
+
+    it('can use firebase Auth from props', () => {
+      expect(() =>
+        render(
+          <React.Suspense fallback={'loading'}>
+            <AuthCheck fallback={<h1>not signed in</h1>} auth={(app.auth() as unknown) as firebase.auth.Auth}>
+              {'signed in'}
+            </AuthCheck>
+          </React.Suspense>
+        )
+      ).not.toThrow();
+    });
+
+    it('renders the fallback if a user is not signed in', async () => {
+      const { getByTestId } = render(<Component />);
+
+      await waitFor(() => expect(getByTestId('signed-out')).toBeInTheDocument());
+
+      await act(async () => {
+        await signIn();
+      });
+
+      await waitFor(() => expect(getByTestId('signed-in')).toBeInTheDocument());
+    });
+
+    it('renders children if a user is logged in', async () => {
+      await act(async () => {
+        await signIn();
+      });
+
+      const { getByTestId } = render(<Component />);
+
+      await waitFor(() => expect(getByTestId('signed-in')).toBeInTheDocument());
+    });
+
+    it('can switch between logged in and logged out', async () => {
+      const { getByTestId } = render(<Component />);
+
+      await waitFor(() => expect(getByTestId('signed-out')).toBeInTheDocument());
+
+      await act(async () => {
+        await signIn();
+      });
+
+      await waitFor(() => expect(getByTestId('signed-in')).toBeInTheDocument());
+
+      await act(async () => {
+        await app.auth().signOut();
+      });
+
+      await waitFor(() => expect(getByTestId('signed-out')).toBeInTheDocument());
+    });
+
+    test.todo('checks requiredClaims');
+  });
+
+  describe('useUser', () => {
+    it('always returns a user if inside an <AuthCheck> component', async () => {
+      const UserDetails = () => {
+        const user = useUser();
+
+        expect(user).not.toBeNull();
+        expect(user).toBeDefined();
+
+        return <h1>Hello</h1>;
+      };
+
+      render(
+        <>
+          <Component>
+            <UserDetails />
+          </Component>
+        </>
+      );
+    });
+
+    test.todo('throws an error if firebase.auth() is not available');
+
+    test.todo('returns the same value as firebase.auth().currentUser()');
+  });
 });
