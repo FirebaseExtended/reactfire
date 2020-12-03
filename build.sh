167279009154bc4b2a01b1b22462fb1891a30aae
James Daniels <jamesdaniels@google.com>
2020-12-02 22:03:44 -0500

    Publish step was fetch wrong version, remove footgun

--- build.sh
+++ build.sh
@@ -1,11 +1,21 @@
 SHORT_SHA=$(git rev-parse --short $GITHUB_SHA)
 TAG_TEST="^refs/tags/v.+$"
+LATEST_TEST="^[^-]*$"
 
 if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
     OVERRIDE_VERSION=${GITHUB_REF/refs\/tags\/v/}
+    if [[ $NPM_VERSION =~ $LATEST_TEST ]]; then
+        NPM_TAG=latest
+    else
+        NPM_TAG=next
+    fi;
 else
     OVERRIDE_VERSION=$(node -e "console.log(require('./package.json').version)")-canary.$SHORT_SHA
+    NPM_TAG=canary
 fi;
 
 npm --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION
-yarn build && mv $(npm pack . | tail -n 1) reactfire.tgz
+yarn build &&
+    mv $(npm pack . | tail -n 1) reactfire.tgz &&
+    echo "npm publish ./reactfire-$GITHUB_RUN_ID/reactfire.tgz --tag $NPM_TAG" > ./publish.sh &&
+    chmod +x ./publish.sh
