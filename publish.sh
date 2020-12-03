be692c5e8cbd1e4c9c23faec767c0a520bae8310
James Daniels <jamesdaniels@google.com>
2020-12-02 19:23:14 -0500

    Version during the buildstep

--- publish.sh
+++ publish.sh
@@ -1,18 +1,14 @@
 LATEST_TEST="^[^-]*$"
-SHORT_SHA=$(git rev-parse --short $GITHUB_SHA)
-TAG_TEST="^refs/tags/v.+$"
+CANARY_TEST="-canary."
 
-if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
-    OVERRIDE_VERSION=${GITHUB_REF/refs\/tags\/v/}
-    if [[ $OVERRIDE_VERSION =~ $LATEST_TEST ]]; then
-        NPM_TAG=latest
-    else
-        NPM_TAG=next
-    fi;
-else
-    OVERRIDE_VERSION=$(npm version | sed -n "s/. reactfire: '\(.*\)',/\1/p")-canary.$SHORT_SHA
+NPM_VERSION=$(npm version | sed -n "s/. reactfire: '\(.*\)',/\1/p")
+
+if [[ $NPM_VERSION =~ $LATEST_TEST ]]; then
+    NPM_TAG=latest
+elif [[ $NPM_VERSION =~ $CANARY_TEST ]]; then
     NPM_TAG=canary
+else
+    NPM_TAG=next
 fi;
 
-npm --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION
-npm publish . --tag $NPM_TAG
+npm publish reactfire-$GITHUB_RUN_ID/reactfire.tgz --tag $NPM_TAG
