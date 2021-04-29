96bf1d7f92ab7cd993168302b43e99c55e75fb8d
jhuleatt <jhuleatt@google.com>
2021-04-29 12:22:11 -0400

    update test commands

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
+// https://jestjs.io/docs/configuration#options
 module.exports = {
-  verbose: false,
   setupFilesAfterEnv: ['./test/setupTests.ts'],
-  testEnvironment: './test/custom-jest-environment.js'
+  testEnvironment: './test/custom-jest-environment.js',
+  forceExit: true // jest hangs if we don't have this
 };
