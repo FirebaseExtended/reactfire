8f399ae9b8c02f852f75f7639d35d3f5960c57a5
jhuleatt <jhuleatt@google.com>
2021-05-05 09:57:44 -0400

    index.ts to v9

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import firebase from 'firebase/app';
 import { SuspenseSubject } from './SuspenseSubject';
 
+import type {Query as FirestoreQuery} from 'firebase/firestore';
+import type {Query as DatabaseQuery} from 'firebase/database';
+
 export type ReactFireGlobals = {
-  _reactFireDatabaseCachedQueries: Array<firebase.database.Query>;
-  _reactFireFirestoreQueryCache: Array<firebase.firestore.Query>;
+  _reactFireDatabaseCachedQueries: Array<DatabaseQuery>;
+  _reactFireFirestoreQueryCache: Array<FirestoreQuery>;
   _reactFirePreloadedObservables: Map<string, SuspenseSubject<any>>;
 };
 
