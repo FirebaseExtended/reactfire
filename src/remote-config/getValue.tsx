77b99c1d216700cba38d7a45453c2b2cd7a56a53
jhuleatt <jhuleatt@google.com>
2021-05-05 11:44:37 -0400

    convert remote config

--- src/remote-config/getValue.tsx
+++ src/remote-config/getValue.tsx
@@ -1,7 +1,14 @@
 import { Observable } from 'rxjs';
+import {
+  ensureInitialized,
+  getValue as rcGetValue,
+  getString as rcGetString,
+  getNumber as rcGetNumber,
+  getBoolean as rcGetBoolean,
+  getAll as rcGetAll,
+} from 'firebase/remote-config';
 
-type RemoteConfig = import('firebase/app').default.remoteConfig.RemoteConfig;
-type RemoteConfigValue = import('firebase/app').default.remoteConfig.Value;
+import type { RemoteConfig, Value as RemoteConfigValue } from 'firebase/remote-config';
 
 export type AllParameters = {
   [key: string]: RemoteConfigValue;
@@ -16,7 +23,7 @@ interface ParameterSettings<T> {
 // TODO(davideast): Replace with RxFire functions when they land
 function parameter$<T>({ remoteConfig, key, getter }: ParameterSettings<T>): Observable<T> {
   return new Observable((subscriber) => {
-    remoteConfig.ensureInitialized().then(() => {
+    ensureInitialized(remoteConfig).then(() => {
       // 'this' for the getter loses context in the next()
       // call, so it needs to be bound.
       subscriber.next(getter.bind(remoteConfig)(key));
@@ -25,27 +32,27 @@ function parameter$<T>({ remoteConfig, key, getter }: ParameterSettings<T>): Obs
 }
 
 export function getValue(remoteConfig: RemoteConfig, key: string) {
-  const getter = remoteConfig.getValue;
+  const getter = () => rcGetValue(remoteConfig, key);
   return parameter$({ remoteConfig, key, getter });
 }
 
 export function getString(remoteConfig: RemoteConfig, key: string) {
-  const getter = remoteConfig.getString;
+  const getter = () => rcGetString(remoteConfig, key);
   return parameter$<string>({ remoteConfig, key, getter });
 }
 
 export function getNumber(remoteConfig: RemoteConfig, key: string) {
-  const getter = remoteConfig.getNumber;
+  const getter = () => rcGetNumber(remoteConfig, key);
   return parameter$<number>({ remoteConfig, key, getter });
 }
 
 export function getBoolean(remoteConfig: RemoteConfig, key: string) {
-  const getter = remoteConfig.getBoolean;
+  const getter = () => rcGetBoolean(remoteConfig, key);
   return parameter$<boolean>({ remoteConfig, key, getter });
 }
 
 export function getAll(remoteConfig: RemoteConfig) {
-  const getter = remoteConfig.getAll;
+  const getter = () => rcGetAll(remoteConfig);
   // No key is needed for getAll()
   return parameter$<AllParameters>({ remoteConfig, key: '', getter });
 }
