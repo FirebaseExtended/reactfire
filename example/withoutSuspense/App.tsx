f588028f14d3e5008925f4f5c7371e59109a4c6b
jhuleatt <3759507+jhuleatt@users.noreply.github.com>
2021-07-02 08:56:45 -0400

    Auth example is working!

--- example/withoutSuspense/App.tsx
+++ example/withoutSuspense/App.tsx
@@ -1,24 +1,31 @@
 import * as React from 'react';
-import { FirebaseAppProvider } from 'reactfire';
+import { FirebaseAppProvider, AuthProvider } from 'reactfire';
 import { Card } from '../display/Card';
 import { Auth } from './Auth';
-import { Firestore } from './Firestore';
-import { RealtimeDatabase } from './RealtimeDatabase';
-import { RemoteConfig } from './RemoteConfig';
-import { Storage } from './Storage';
+// import { Firestore } from './Firestore';
+// import { RealtimeDatabase } from './RealtimeDatabase';
+// import { RemoteConfig } from './RemoteConfig';
+// import { Storage } from './Storage';
 
 // Import auth directly because most components need it
 // Other Firebase libraries can be lazy-loaded as-needed
-import 'firebase/auth';
+import { initializeApp } from '@firebase/app';
+import { getAuth } from 'firebase/auth';
+
 
 export const App = ({ firebaseConfig }: { firebaseConfig: { [key: string]: unknown } }) => {
+  
+  const firebaseApp = initializeApp(firebaseConfig);
+  const auth = getAuth(firebaseApp);
+  
   return (
     <div className="flex flex-wrap justify-around p-4">
-      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <FirebaseAppProvider firebaseApp={firebaseApp}>
+        <AuthProvider sdk={auth}>
         <Card title="Authentication">
           <Auth />
         </Card>
-        <Card title="Firestore">
+        {/* <Card title="Firestore">
           <Firestore />
         </Card>
         <Card title="Realtime Database">
@@ -29,7 +36,8 @@ export const App = ({ firebaseConfig }: { firebaseConfig: { [key: string]: unkno
         </Card>
         <Card title="Storage">
           <Storage />
-        </Card>
+        </Card> */}
+        </AuthProvider>
       </FirebaseAppProvider>
     </div>
   );
