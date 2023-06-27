f9c660e642fed35ad7b32615ce3f9ada01985dbf
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2023-06-27 15:14:35 -0400

    add bundle visualizer to example

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    // Visualize the bundle in `stats.html`.
+    // Helps make sure we aren't pulling in extra deps
+    visualizer({ template: 'treemap' }),
+  ],
+});
